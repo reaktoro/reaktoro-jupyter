8b783db04aa82ec10e4d664a36cec738ea29a994
Svetlana Kyas <mtsveta@gmail.com>
2019-12-11 21:17:01 +0100

    Pycharm marks are removed

--- scripts/tutorial-equilibrium-basics.py
+++ scripts/tutorial-equilibrium-basics.py
@@ -22,28 +22,28 @@
 # %% [markdown]
 # We start by importing everything from the `reaktoro` package:
 
-# %% pycharm={"is_executing": false}
+# %%
 from reaktoro import *
 
 # %% [markdown]
 # Next, we need a thermodynamic database that enable us to compute thermodynamic properties of species and reactions. 
 # To initialize it, we execute
 
-# %% pycharm={"is_executing": false}
+# %%
 db = Database('supcrt98.xml')
 
 # %% [markdown]
 # To indicate phases as well as species, which may potentially exist in them at equilibrium, we use 
 # `ChemicalEditor`. 
 
-# %% pycharm={"is_executing": false}
+# %%
 editor = ChemicalEditor(db)
 
 # %% [markdown]
 # We consider an aqueous phase with species that can be formed with given chemical element symbols:
 #  
 
-# %% pycharm={"is_executing": false}
+# %%
 editor.addAqueousPhaseWithElements('H O Na Cl C Ca Mg Si');
 
 # %% [markdown]
@@ -52,35 +52,35 @@ editor.addAqueousPhaseWithElements('H O Na Cl C Ca Mg Si');
 # applications , you might want to manually specify the chemical species of each phase in your chemical system. This 
 # can be achieved by providing explicit list of species  names as it is done for gaseous species below:
 
-# %% pycharm={"is_executing": false}
+# %%
 editor.addGaseousPhase('H2O(g) CO2(g)');  
 
-# %% [markdown] pycharm={"name": "#%% md\n"}
+# %% [markdown]
 # Finally, we consider all possible pure minerals that could exist in our equilibrium calculations:
 
-# %% pycharm={"is_executing": false}
+# %%
 editor.addMineralPhase('Halite');
 editor.addMineralPhase('Calcite');
 editor.addMineralPhase('Magnesite');
 editor.addMineralPhase('Dolomite');
 editor.addMineralPhase('Quartz');
 
-# %% [markdown] pycharm={"name": "#%% md\n"}
+# %% [markdown]
 # Next follows an important step, where we create the chemical system with the information so far collected in the `ChemicalEditor` object `editor`:
 
-# %% pycharm={"is_executing": false}
+# %%
 system = ChemicalSystem(editor) 
 
-# %% [markdown] pycharm={"name": "#%% md\n"}
+# %% [markdown]
 # We use `EquilibriumProblem` to specify the conditions, at which our system should be in equilibrium. 
 
-# %% pycharm={"is_executing": false}
+# %%
 problem = EquilibriumProblem(system) 
 
 # %% [markdown]
 # In particular, we can specify temperature and pressure as well as the initial condition for substance amounts
 
-# %% pycharm={"is_executing": false}
+# %%
 problem.setTemperature(70, 'celsius');
 problem.setPressure(100, 'bar');
 
@@ -91,75 +91,75 @@ problem.add('CaCO3', 10.0, 'g');
 problem.add('MgCO3', 5.0, 'g');
 problem.add('Quartz', 1.0, 'mol');
 
-# %% [markdown] pycharm={"name": "#%% md\n"}
+# %% [markdown]
 # To perform a fast Gibbs energy minimization calculation and calculate the chemical equilibrium `state`, we have to 
 # equilibrate the object `problem` with similarly named method:
 
-# %% pycharm={"is_executing": false}
+# %%
 state = equilibrate(problem)  
 
 # %% [markdown]
 # We can access the amounts of species at equilibrium with method `speciesAmounts()`, i.e., 
 
-# %% pycharm={"is_executing": false}
+# %%
 state.speciesAmounts()  
 
 # %% [markdown]
 # To print the name and the amount (in mol) of each species in the obtained chemical system, we execute the following 
 # loop:
 
-# %% pycharm={"name": "#%%\n", "is_executing": false}
+# %%
 for species in system.species():
     print(f'{species.name():>15} = {state.speciesAmount(species.name())}')
 
 # %% [markdown]
 # You can also output the chemical state to a file (check the results folder on the left pane of jupyter lab!)
 
-# %% pycharm={"name": "#%%\n", "is_executing": false}
+# %%
 state.output('results/equilibrium-basics-state.txt')
 
 # %% [markdown]
 # Use `ChemicalProperties` to obtain all chemical and thermodynamic properties of a chemical system at some 
 # equilibrium state by
 
-# %% pycharm={"is_executing": false}
+# %%
 properties = ChemicalProperties(system)
 
 # %% [markdown]
 # and compute the chemical properties of the system at given state
 
-# %% pycharm={"is_executing": false}
+# %%
 properties.update(state.temperature(), state.pressure(), state.speciesAmounts())  
 
-# %% [markdown] pycharm={"name": "#%% md\n"}
+# %% [markdown]
 # Alternatively, one can also use the shorter notation below, at the expense of creating a new `ChemicalProperties` object
 #  at each call, instead of updating an existing one
 
-# %% pycharm={"is_executing": false}
+# %%
 properties = state.properties()
 
-# %% [markdown] pycharm={"name": "#%% md\n"}
+# %% [markdown]
 # To evaluate some of the obtained properties, let's print the activities of each species. First, we get the values of 
 # $\ln$ activities of the species by a call `.val`. **Note!** If you use `.ddT`, `.ddP`, or `.ddn` instead of `.val`, 
 # you'll get, respectively, the T, P, n derivatives. 
 
-# %% pycharm={"is_executing": false}
+# %%
 lna = properties.lnActivities().val
 
-# %% [markdown] pycharm={"name": "#%% md\n"}
+# %% [markdown]
 # To compute the actual activities, not the natural logo of them, we use
 
-# %% pycharm={"name": "#%%\n", "is_executing": false}
+# %%
 a = numpy.exp(lna)  
 
 for i, species in enumerate(system.species()):
     print(f'{species.name():>15} = {a[i]}')
 
-# %% [markdown] pycharm={"name": "#%% md\n"}
+# %% [markdown]
 # Let's create a pH function that determines the pH of the aqueous solution given the chemical properties of the system 
 # (this will be soon simplified!)
 
-# %% pycharm={"is_executing": false}
+# %%
 evaluate_pH = ChemicalProperty.pH(system)
 pH = evaluate_pH(properties)
 print(f'The pH of the aqueous phase is {pH.val}.')
--- scripts/tutorial-thermodynamic-properties.py
+++ scripts/tutorial-thermodynamic-properties.py
@@ -21,30 +21,30 @@
 # > *If your main interest is on computing thermodynamic properties, rather than chemical equilibrium and kinetics modeling, 
 # > you may want to check [ThermoFun](https://thermohub.org/thermofun/thermofun/), which is an excellent project dedicated for this task.*
 
-# %% [markdown] pycharm={"name": "#%% md\n"}
+# %% [markdown]
 # First, we import everything from the `reaktoro` package by
 
-# %% pycharm={"is_executing": false}
+# %%
 from reaktoro import *  
 
-# %% [markdown] pycharm={"name": "#%% md\n"}
+# %% [markdown]
 # We need a thermodynamic database that enables us to compute thermodynamic properties of species and reactions. The 
 # object `bd` is an instance of the class `Database`:
 
-# %% pycharm={"is_executing": false}
+# %%
 db = Database('supcrt98.xml')  
 
 # %% [markdown]
 # To access the thermodynamic property evaluations, we create a `Thermo` object:
 
-# %% pycharm={"is_executing": false}
+# %%
 thermo = Thermo(db)
 
-# %% [markdown] pycharm={"name": "#%% md\n"}
-# For computing the standard Gibbs energy of $\mathrm{Na+}$ at given temperature and pressure, we use function 
+# %% [markdown]
+# For computing the standarGIT d Gibbs energy of $\mathrm{Na+}$ at given temperature and pressure, we use function
 # `standardPartialMolarGibbsEnergy()` 
 
-# %% pycharm={"name": "#%%\n", "is_executing": false}
+# %%
 T = 300.0  # temperature in K
 P = 10.0e5  # pressure in Pa (equivalent to 10 bar)
 
@@ -53,13 +53,13 @@ G0 = thermo.standardPartialMolarGibbsEnergy(T, P, 'Na+')
 # %% [markdown]
 # Similarly, we can compute the $\log(K)$ of given reaction at given $(T, P)$ by using function `logEquilibriumConstant()`: 
 
-# %% pycharm={"name": "#%%\n", "is_executing": false}
+# %%
 logK = thermo.logEquilibriumConstant(T, P, 'H2O(l) = H+ + OH-')  
 
 # %% [markdown]
 # To print computed values, `.val` must be used:
 
-# %% pycharm={"name": "#%%\n", "is_executing": false}
+# %%
 print(f'G0(Na+) = {G0.val} J/mol')  # use G0.ddT or G0.ddP to get temperature and pressure derivatives
 print(f'logK(H2O(l) = H+ + OH-) = {logK.val}')  # use logK.ddT or logK.ddP to get temperature and pressure derivatives
 
