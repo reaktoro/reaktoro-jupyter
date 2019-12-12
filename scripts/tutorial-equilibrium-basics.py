22471f33134ca6099c59bdc74a469e0f6e702f2e
Allan Leal <allanmulin@gmail.com>
2019-12-12 11:59:20 +0100

    Use light format for equilibrium tutorial

--- scripts/tutorial-equilibrium-basics.py
+++ scripts/tutorial-equilibrium-basics.py
@@ -2,167 +2,130 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: notebooks//ipynb,scripts//py:percent
+#     cell_metadata_filter: -all
+#     formats: py:light,../notebooks//ipynb
 #     text_representation:
 #       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
+#       format_name: light
+#       format_version: '1.5'
 #       jupytext_version: 1.3.0
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
 # ---
 
-# %% [markdown]
 # # Performing a chemical equilibrium calculation
-#
-# This tutorial demonstrates how to use Reaktoro to perform a chemical equilibrium calculation.
 
-# %% [markdown]
-# We start by importing everything from the `reaktoro` package:
+# This tutorial demonstrates how to use Reaktoro to perform a chemical equilibrium calculation. We start by importing the `reaktoro` package:
 
-# %%
 from reaktoro import *
 
-# %% [markdown]
-# Next, we need a thermodynamic database that enable us to compute thermodynamic properties of species and reactions. 
-# To initialize it, we execute
+# Next, we need a thermodynamic database that enable us to compute thermodynamic properties of species and reactions. To initialize it, we execute
 
-# %%
-db = Database('supcrt98.xml')
+db = Database("supcrt98.xml")
 
-# %% [markdown]
-# To indicate phases as well as species, which may potentially exist in them at equilibrium, we use 
-# `ChemicalEditor`. 
+# To indicate the phases of interest (as well as their species) that may potentially exist at equilibrium, we create an object of class `ChemicalEditor`:
 
-# %%
 editor = ChemicalEditor(db)
 
-# %% [markdown]
-# We consider an aqueous phase with species that can be formed with given chemical element symbols:
-#  
+# We consider an aqueous phase composed of all aqueous species in the database that can be formed with the given chemical elements below:
 
-# %%
-editor.addAqueousPhaseWithElements('H O Na Cl C Ca Mg Si');
+editor.addAqueousPhaseWithElements("H O Na Cl C Ca Mg Si")
 
-# %% [markdown]
-# An automatic search for chemical species can result in a large number of species in the phase, potentially causing 
-# the chemical reaction calculations to be more computationally expensive. If you are using Reaktoro in demanding  
-# applications , you might want to manually specify the chemical species of each phase in your chemical system. This 
-# can be achieved by providing explicit list of species  names as it is done for gaseous species below:
+# > **Note:** This automatic selection of chemical species for a phase can result in a large number of them. This potentially increases the computing cost of the chemical reaction calculations. If you are using Reaktoro in demanding applications, you might want to manually specify the chemical species of each phase in your chemical system. This can be achieved by providing an explicit list of species names, e.g., `editor.addAqueousPhase("H2O(l) H+ OH- CO2(aq)")`. Note, however, that care is required here to ensure relevant species are not missing. The just given example is a bad one in fact, with important species such as `HCO3-` and `CO3--` missing in the list!.
 
-# %%
-editor.addGaseousPhase('H2O(g) CO2(g)');  
+# We are interested in a gaseous phase containing exactly the following gases (which may or not exist in positive amounts at the end of our equilibrium calculation later!):
 
-# %% [markdown]
-# Finally, we consider all possible pure minerals that could exist in our equilibrium calculations:
+editor.addGaseousPhase("H2O(g) CO2(g)")
 
-# %%
-editor.addMineralPhase('Halite');
-editor.addMineralPhase('Calcite');
-editor.addMineralPhase('Magnesite');
-editor.addMineralPhase('Dolomite');
-editor.addMineralPhase('Quartz');
+# Finally, we consider some pure minerals that could exist in positive amounts in our equilibrium calculations:
 
-# %% [markdown]
+editor.addMineralPhase("Halite")
+editor.addMineralPhase("Calcite")
+editor.addMineralPhase("Magnesite")
+editor.addMineralPhase("Dolomite")
+editor.addMineralPhase("Quartz")
+
 # Next follows an important step, where we create the chemical system with the information so far collected in the `ChemicalEditor` object `editor`:
 
-# %%
-system = ChemicalSystem(editor) 
+system = ChemicalSystem(editor)
 
-# %% [markdown]
-# We use `EquilibriumProblem` to specify the conditions, at which our system should be in equilibrium. 
+# We use class `EquilibriumProblem` to specify the conditions at which our system should be in equilibrium.
 
-# %%
-problem = EquilibriumProblem(system) 
+problem = EquilibriumProblem(system)
 
-# %% [markdown]
 # In particular, we can specify temperature and pressure as well as the initial condition for substance amounts
 
-# %%
-problem.setTemperature(70, 'celsius');
-problem.setPressure(100, 'bar');
+problem.setTemperature(70, "celsius")
+problem.setPressure(100, "bar")
+problem.add("H2O", 1.0, "kg")
+problem.add("CO2", 2.0, "mol")
+problem.add("NaCl", 1.0, "mol")
+problem.add("CaCO3", 10.0, "g")
+problem.add("MgCO3", 5.0, "g")
+problem.add("Quartz", 1.0, "mol")
 
-problem.add('H2O', 1.0, 'kg');
-problem.add('CO2', 2.0, 'mol');
-problem.add('NaCl', 1.0, 'mol');
-problem.add('CaCO3', 10.0, 'g');
-problem.add('MgCO3', 5.0, 'g');
-problem.add('Quartz', 1.0, 'mol');
+# > **Note:** The substance names above can either be chemical formulas, such as CaCO3 and CaCl2, as well as names of species that can be found in the database, such as Quartz. Reaktoro will break down the chemical formulas of the substances and calculate the amount of each chemical element in the system. These element amounts are inputs to the equilibrium calculation. In the future, we will only allow species names to be provided, since this is a safer way of preventing unfeasible elemental mass conditions to be imposed (e.g., there are *x* moles of C and *y* moles of O, and distributing these among the species always produce an excess of either C or O).
 
-# %% [markdown]
-# To perform a fast Gibbs energy minimization calculation and calculate the chemical equilibrium `state`, we have to 
-# equilibrate the object `problem` with similarly named method:
+# We now perform a fast Gibbs energy minimization calculation to compute the chemical equilibrium state of the system at given conditions stored in `problem`. For this, we use the convenient function `equilibrate`:
 
-# %%
-state = equilibrate(problem)  
+state = equilibrate(problem)
 
-# %% [markdown]
-# We can access the amounts of species at equilibrium with method `speciesAmounts()`, i.e., 
+# ## Using class ChemicalState to inspect species amounts
 
-# %%
-state.speciesAmounts()  
+# The result of the `equilibrate` call before, `state`, is an object of class `ChemicalState`. This object contains the temperature, pressure, and amounts of the species at the computed chemical equilibrium state. We can access these properties as follows:
 
-# %% [markdown]
-# To print the name and the amount (in mol) of each species in the obtained chemical system, we execute the following 
-# loop:
+T = state.temperature()
+P = state.pressure()
+n = state.speciesAmounts()
 
-# %%
+print(f"T = {T} K")
+print(f"P = {P} Pa")
+print(f"n = (in mol)\n{n}")
+
+# To print the name of each species and its amount (in mol), we execute the following loop:
+
 for species in system.species():
-    print(f'{species.name():>15} = {state.speciesAmount(species.name())}')
+    name = species.name()
+    amount = state.speciesAmount(name)
+    print(f"{name:>15} = {amount}")
 
-# %% [markdown]
-# You can also output the chemical state to a file (check the results folder on the left pane of jupyter lab!)
+# You can also output the chemical state to a file
 
-# %%
-state.output('results/equilibrium-basics-state.txt')
+state.output("results/state.txt")
 
-# %% [markdown]
-# Use `ChemicalProperties` to obtain all chemical and thermodynamic properties of a chemical system at some 
-# equilibrium state by
+# ## Using the class ChemicalProperties to obtain species activities
 
-# %%
+# If you require chemical properties of a system that depend on temperature (*T*), pressure (*P*), and composition (*n*), then `ChemicalProperties` class is what you need
+
 properties = ChemicalProperties(system)
 
-# %% [markdown]
-# and compute the chemical properties of the system at given state
+# We can compute the chemical properties of the system at the state of equilibrium we found before:
 
-# %%
-properties.update(state.temperature(), state.pressure(), state.speciesAmounts())  
+properties.update(T, P, n)
 
-# %% [markdown]
-# Alternatively, one can also use the shorter notation below, at the expense of creating a new `ChemicalProperties` object
-#  at each call, instead of updating an existing one
+# Alternatively, we could also have done:
 
-# %%
 properties = state.properties()
 
-# %% [markdown]
-# To evaluate some of the obtained properties, let's print the activities of each species. First, we get the values of 
-# $\ln$ activities of the species by a call `.val`. **Note!** If you use `.ddT`, `.ddP`, or `.ddn` instead of `.val`, 
-# you'll get, respectively, the T, P, n derivatives. 
+# > **Note:** The call above creates a new object of `ChemicalProperties` each time. If you are using Reaktoro in a simulator that needs the chemical properties of the system at millions/billions of states each time step, prefer to the `update` method of an existing `ChemicalProperties` object.
 
-# %%
+# Once we have computed the chemical properties, we can query for some of them. Below we get the natural log of the species activities:
+
 lna = properties.lnActivities().val
 
-# %% [markdown]
-# To compute the actual activities, not the natural logo of them, we use
+# > **Note:** The use of `.ddT`, `.ddP`, and `.ddn`, instead of `.val`, extracts the derivatives of the activities (or any other chemical property) with respect to *T*, *P*, and *n*, respectively.
 
-# %%
-a = numpy.exp(lna)  
+# To compute the actual activities (not their natural log), and print them one by one, we do
 
+a = numpy.exp(lna)
 for i, species in enumerate(system.species()):
-    print(f'{species.name():>15} = {a[i]}')
+    print(f"{species.name():>15} = {a[i]}")
 
-# %% [markdown]
-# Let's create a pH function that determines the pH of the aqueous solution given the chemical properties of the system 
-# (this will be soon simplified!)
+# ## Calculating the pH of the aqueous solution
 
-# %%
+# Let's create a pH function that computes the pH of the aqueous solution given the chemical properties of the system (this will be soon simplified!)
+
 evaluate_pH = ChemicalProperty.pH(system)
 pH = evaluate_pH(properties)
-print(f'The pH of the aqueous phase is {pH.val}.')
-print(f'Its sensitivity with respect to speciation, ∂(pH)/∂n, is:')
+print(f"The pH of the aqueous phase is {pH.val}.")
+print(f"Its sensitivity with respect to speciation, ∂(pH)/∂n, is:")
 for i, species in enumerate(system.species()):
-    print(f'{species.name():>15} = {pH.ddn[i]}')
+    print(f"{species.name():>15} = {pH.ddn[i]}")
