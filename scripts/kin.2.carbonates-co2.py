f9666a70bb68be7d1cd4ec86d37117c4303f59fc
Svetlana Kyas <mtsveta@gmail.com>
2020-01-15 09:27:59 +0100

    Add kernel specs

--- scripts/kin.2.carbonates-co2.py
+++ scripts/kin.2.carbonates-co2.py
@@ -8,6 +8,10 @@
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.3.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
 # ---
 
 # # Dissolution of carbonate minerals in a CO<sub>2</sub>-saturated brine
--- scripts/rt.3.coupling-reaktoro-to-transport.py
+++ scripts/rt.3.coupling-reaktoro-to-transport.py
@@ -9,6 +9,10 @@
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.3.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
 # ---
 
 # # Coupling Reaktoro into other reactive transport codes
