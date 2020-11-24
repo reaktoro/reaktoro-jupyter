e9096c5c7e0619f947656d8a61e5ad342c816a85
Svetlana Kyas <mtsveta@gmail.com>
2020-11-24 14:05:55 +0100

    Polish tutorial with solubility of table salt in table water

--- scripts/eq.sodium-chloride-solubility-in-water.py
+++ scripts/eq.sodium-chloride-solubility-in-water.py
@@ -10,9 +10,9 @@
 #       jupytext_version: 1.3.2
 # ---
 
-# # Mixing of the table salt with water
+# # Solubility of the table salt with water
 #
-# Importing the 'reaktoro' package:
+# Importing the **reaktoro** package:
 
 from reaktoro import *
 
@@ -30,13 +30,9 @@ editor.addMineralPhase("Halite") # sodium chloride (solid)
 
 system = ChemicalSystem(editor)
 
-# To evaluate the content of the system, we type:
+# To evaluate the content of the chemical system:
 
 print("system = ", system)
-
-# We see that all the discussed during the lecture species are there, i.e.,
-# H<sub>2</sub>O, H<sup>+</sup>. OH<sup>-</sup>, O<sub>2</sub>(aq),
-# H<sub>2</sub>(aq), Na<sup>+</sup>, Cl<sup>-</sup>, NaCl(aq), HCl(aq), NaOH(aq) among others.
 
 # Initializing chemical problem:
 
@@ -49,7 +45,7 @@ problem.add("NaCl", 1.0, "mg") # sodium chloride / table salt
 # After mixing water and salt, the following reactions start:
 #
 # \begin{alignat}{4}
-# {\rm NaCl(s)} & \rightleftharpoons- {\rm Na}^+ + {\rm Cl}^- &\qquad& (1) \\
+# {\rm NaCl(s)} & \rightleftharpoons {\rm Na}^+ + {\rm Cl}^- &\qquad& (1) \\
 # {\rm Na}^+ + {\rm Cl}^- &  \rightleftharpoons {\rm NaCl(aq)} &\qquad& (2) \\
 # {\rm H_2O} & \rightleftharpoons {\rm H}^+ + {\rm 0H}^- &\qquad& (3) \\
 # \end{alignat}
@@ -58,9 +54,9 @@ problem.add("NaCl", 1.0, "mg") # sodium chloride / table salt
 # In (2), ions of sodium and chloride combine aqueous sodium chloride, NaCl(aq).
 # In reaction (3), water provides solvent species for other solutions.
 # Another species that can be forming are: HCl(aq), NaOH(aq) by the following reactions:
-# $${\rm H}^{+} + {\rm Cl}^- \rightleftharpoons {\rm HCl(aq)}$$
+# $${\rm H}^{+} + {\rm Cl}^- \rightleftharpoons {\rm HCl(aq)} \quad \mbox{or} \quad {\rm Na}^{+} + {\rm OH}^- \rightleftharpoons {\rm NaOH(aq)}.$$
 #
-# Let us equilibrate the problem:
+# We equilibrate the above-defined problem:
 
 state = equilibrate(problem)
 
@@ -71,12 +67,13 @@ print("state = ", state)
 
 # We see that the Halite mineral has practically zero amount and `under stable` state
 # in the resulting mixture, which indicates that it was completely dissolved in water.
-# Let us add some more salt
+# However, let us add some more salt:
 
 problem.add("NaCl", 1.0, "kg") # additional 1kg of salt
 state = equilibrate(problem)
 print("state = ", state)
 
-# To evaluate the amount of a single (or a particular species'), one can use:
+# We see that with more added salt, we obtained 4.08036 mol of the precipitated halite. It is also confirmed by the
+# stability of the mineral phase. To evaluate the amount of a particular species, one can use:
 
 print(f"Amount of NaCl(s) : {state.speciesAmount('Halite')}")
